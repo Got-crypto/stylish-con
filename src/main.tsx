0eb779bef9bfd6a9249d47d62b826351d922604e
Got-crypto <pecpalmplay@gmail.com>
2024-09-03 02:00:55 +0200

    learning a little bit react-router-dom.v6

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ErrorElement from './components/ErrorElement.tsx'
+import './index.scss'
+
 import App from './App.tsx'
-import './index.css'
+import { About } from "./pages"
+
+const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if(!publishableKey) {
+  console.error("Publishable key not found")
+}
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorElement />,
+    children: [
+      {
+        path: 'first',
+        element: <p>First Element</p>
+      },
+      {
+        path: 'second',
+        element: <p>Second Element</p>
+      }
+    ]
+  },
+  {
+    path: '/about',
+    element: <About />,
+    errorElement: <ErrorElement />
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={routes} />
 )
---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+import About from "./About";
+import Home from "./Home";
+
+export {
+    About, Home
+};
